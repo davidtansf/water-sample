3dc8352fcfa5b937e97190a7ec7750f505240ebc
David Tan <davidtansf@gmail.com>
2015-09-21 01:52:40 -0700

    [feature] query for new water samples

--- client/controllers/QueryController.js
+++ client/controllers/QueryController.js
@@ -1,5 +1,34 @@
 angular.module('water.querycontroller', [])
 
-.controller('QueryController', ['$scope', '$state', function($scope, $state) {
+.controller('QueryController', ['$scope', '$http', function($scope, $http) {
+  $scope.getSample = function(id, callback) {
+    console.log($scope.sample_id);
+    $http({
+      url: '/sample',
+      method: 'GET',
+      params: { 'id' : $scope.sample_id }
+    })
+    .success(function(data){
+      console.log(data);
+      $scope.displaySample(data);
+    });
+  };
 
+  $scope.displaySample = function(data) {
+    var $results = [];
+    if (data) {
+      _.each(data, function(value, key) {
+
+        $whole = $('<p></p>').text(key + ": " + value);
+        $results.push($whole);
+      });
+    } else {
+      $whole = $('<p></p>').text("id: " + null);
+      $results.push($whole);
+    }
+    console.log($results);
+    $('#holder').attr('id','sample');
+    $('#sample').find('ul').html($results);
+
+  };
 }]);
--- client/controllers/SampleController.js
+++ client/controllers/SampleController.js
@@ -1,5 +1,27 @@
 angular.module('water.samplecontroller', [])
 
-.controller('SampleController', ['$scope', '$state', function($scope, $state) {
+.controller('SampleController', ['$scope', '$http', function($scope, $http) {
+  $scope.getSample = function(id, callback) {
+    console.log($scope.sample_id);
+    $http({
+      url: '/sample',
+      method: 'GET',
+      data: { 'id' : $scope.sample_id }
+    })
+    .success(function(data){
+        $scope.displaySample(data.results);
+    });
+  };
+
+  $scope.displaySample = function(data) {
+    var $results = [];
+    _.each(data, function(value, key) {
+      $whole = $('<p></p>').text(key + ": " + value);
+      $results.push($whole);
+    });
+    $('#holder').attr('id','sample');
+    $('#sample').find('ul').html($results);
+
+  };
 
 }]);
