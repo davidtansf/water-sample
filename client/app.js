774510fe27936b30981b393779bbad199a666453
David Tan <davidtansf@gmail.com>
2015-09-21 02:23:54 -0700

    [cleanup] remove jquery code, refactor functions

--- client/app.js
+++ client/app.js
@@ -33,165 +33,177 @@ angular.module('water', ['ui.router',
 
 }]);
 
-var app, WaterSample, a;
-$(function() {
-  app = {
+// var app, WaterSample;
+// $(function() {
+//   app = {
 
-    init: function() {
-      $(".submit").on('submit',function(event) {
-        event.preventDefault();
-        var id = $(".input").val();
-        if (id) {
-          a = new WaterSample(id);
-        }
-       $(".input").val("");
-      });
-      app.fetch();
-    },
+//     storage: undefined,
 
-    fetch: function() {
-      $.ajax('http://localhost:3000/data', {
-        contentType: 'application/json',
-        type: 'GET',
-        success: function(data){
-          app.displayData(data);
-        },
-        error: function(data) {
-          throw new Error('Error: Did not fetch data');
-        }
-      });
-    },
+//     init: function() {
+//       $(".submit").on('submit',function(event) {
+//         event.preventDefault();
+//         var id = $(".input").val();
+//         if (id) {
+//           app.storage = new WaterSample(id);
+//         }
+//        $(".input").val("");
+//       });
+//       app.fetch();
+//     },
 
-    displayData: function(data) {
-      var $results = [];
-      data = JSON.parse(data);
-      _.each(data.results, function(sample) {
-        $whole = $('<p></p>').text(sample.id + " " + sample.site);
-        $results.push($whole);
-      });
-      $('#main').find('ul').html($results);
-    },
+//     fetch: function() {
+//       $.ajax('http://localhost:3000/data', {
+//         contentType: 'application/json',
+//         type: 'GET',
+//         success: function(data){
+//           app.displayData(data);
+//         },
+//         error: function(data) {
+//           throw new Error('Error: Did not fetch data');
+//         }
+//       });
+//     },
 
-    displaySample: function(data) {
-      var $results = [];
-      _.each(data, function(value, key) {
-        $whole = $('<p></p>').text(key + ": " + value);
-        $results.push($whole);
-      });
-      $('#holder').attr('id','sample');
-      $('#sample').find('ul').html($results);
-    },
+//     displayData: function(data) {
+//       var $results = [];
+//       data = JSON.parse(data);
+//       _.each(data.results, function(sample) {
+//         $whole = $('<p></p>').text(sample.id + " " + sample.site);
+//         $results.push($whole);
+//       });
+//       $('#main').find('ul').html($results);
+//     },
 
-    getSample: function(id, callback) {
-      $.ajax({
-        url: 'http://localhost:3000/sample',
-        contentType: 'application/json',
-        type: 'GET',
-        data: { 'id' : id },
-        success: function(data){
-          callback(data);
-        },
-        error: function(data){
-          throw new Error('getSample Error!');
-        }
-      });
-    },
+//     displaySample: function(data, flag) {
+//       var $results = [];
+//       _.each(data, function(value, key) {
+//         $whole = $('<p></p>').text(key + ": " + value);
+//         $results.push($whole);
+//       });
+//       if (!flag) {
+//         $factorbtn = $('<form class="factorsubmit"> \
+//             <div id="factorbutton"><input id="factorButton" \
+//             type="submit" value="Get Factor"></div></form>');
+//         $results.push($factorbtn);
+//       }
+//       $('#holder').attr('id','sample');
+//       $('#sample').find('ul').html($results);
+//       $(".factorsubmit").on('submit',function(event) {
+//         event.preventDefault();
+//         app.displaySample(app.storage.to_hash(true), true);
+//       });
+//     },
 
-    getFactor: function(id, callback) {
-      var factor;
-      $.ajax({
-        async: false,
-        url: 'http://localhost:3000/factor',
-        contentType: 'application/json',
-        type: 'GET',
-        data: {'id' : id },
-        success: function(data){
-          factor = callback(data);
-        },
-        error: function(data){
-          throw new Error('getFactor Error!');
-        }
-      });
-      return factor;
-    },
+//     getSample: function(id, callback) {
+//       $.ajax({
+//         url: 'http://localhost:3000/sample',
+//         contentType: 'application/json',
+//         type: 'GET',
+//         data: { 'id' : id },
+//         success: function(data){
+//           callback(data);
+//         },
+//         error: function(data){
+//           throw new Error('getSample Error!');
+//         }
+//       });
+//     },
 
-    toHashAll: function(callback) {
-      var obj;
-      $.ajax({
-        async: false,
-        url: 'http://localhost:3000/hash',
-        contentType: 'application/json',
-        type: 'GET',
-        success: function(data){
-          obj = callback(JSON.parse(data));
-        },
-        error: function(data){
-          throw new Error('toHashAll Error!');
-        }
-      });
-      return obj;
-    }
-  };
+//     getFactor: function(id, callback) {
+//       var factor;
+//       $.ajax({
+//         async: false,
+//         url: 'http://localhost:3000/factor',
+//         contentType: 'application/json',
+//         type: 'GET',
+//         data: {'id' : id },
+//         success: function(data){
+//           factor = callback(data);
+//         },
+//         error: function(data){
+//           throw new Error('getFactor Error!');
+//         }
+//       });
+//       return factor;
+//     },
 
-  WaterSample = function(id) {
-    var find = function(id) {
-      app.getSample(id, function(data) {
-        if (data) {
-          data = JSON.parse(data);
-          this.id = data.id;
-          this.site = data.site;
-          this.chloroform = data.chloroform;
-          this.bromoform = data.bromoform;
-          this.bromodichloromethane = data.bromodichloromethane;
-          this.dibromichloromethane = data.dibromichloromethane;
-        } else {
-          this.id = null;
-        }
-        app.displaySample(this);
-      }.bind(this));
-    }.bind(this);
+//     toHashAll: function(callback) {
+//       var obj;
+//       $.ajax({
+//         async: false,
+//         url: 'http://localhost:3000/hash',
+//         contentType: 'application/json',
+//         type: 'GET',
+//         success: function(data){
+//           obj = callback(JSON.parse(data));
+//         },
+//         error: function(data){
+//           throw new Error('toHashAll Error!');
+//         }
+//       });
+//       return obj;
+//     }
+//   };
 
-    find(id);
+//   WaterSample = function(id) {
+//     var find = function(id) {
+//       app.getSample(id, function(data) {
+//         if (data) {
+//           data = JSON.parse(data);
+//           this.id = data.id;
+//           this.site = data.site;
+//           this.chloroform = data.chloroform;
+//           this.bromoform = data.bromoform;
+//           this.bromodichloromethane = data.bromodichloromethane;
+//           this.dibromichloromethane = data.dibromichloromethane;
+//           app.displaySample(this);
+//         } else {
+//           this.id = null;
+//           app.displaySample(this, true);
+//         }
+//       }.bind(this));
+//     }.bind(this);
 
-  };
+//     find(id);
 
-  WaterSample.prototype.factor = function(factor_id) {
+//   };
 
-    return app.getFactor(factor_id, function(scheme) {
-      if (scheme) {
-        return this.calcFactor(this, JSON.parse(scheme));
-      }
-      return null;
-    }.bind(this));
+//   WaterSample.prototype.factor = function(factor_id) {
+//     return app.getFactor(factor_id, function(scheme) {
+//       if (scheme) {
+//         return this.calcFactor(this, JSON.parse(scheme));
+//       }
+//       return null;
+//     }.bind(this));
+//   };
 
-  };
+//   WaterSample.prototype.to_hash = function(flag) {
+//     if (!flag) {
+//       return _.mapObject(this, _.identity);
+//     } else {
+//       return app.toHashAll(function(data) {
+//         var obj = _.mapObject(this, _.identity);
 
-  WaterSample.prototype.to_hash = function(flag) {
-    if (!flag) {
-      return _.mapObject(this, _.identity);
-    } else {
-      return app.toHashAll(function(data) {
-        var obj = _.mapObject(this, _.identity);
+//         _.each(data, function(scheme) {
+//           var factorkey = 'factor_' + scheme.id;
+//           obj[factorkey] = this.calcFactor(this, scheme);
+//         }.bind(this));
 
-        _.each(data, function(scheme) {
-          var factorkey = 'factor_' + scheme.id;
-          obj[factorkey] = this.calcFactor(this, scheme);
-        }.bind(this));
+//         return obj;
 
-        return obj;
+//       }.bind(this));
+//     }
+//   };
 
-      }.bind(this));
-    }
-  };
+//   WaterSample.prototype.calcFactor = function(sample, model) {
+//     var factor = 0;
+//     factor += sample.chloroform * model.chloroform_weight;
+//     factor += sample.bromoform * model.bromoform_weight;
+//     factor += sample.bromodichloromethane * model.bromodichloromethane_weight;
+//     factor += sample.dibromichloromethane * model.dibromichloromethane_weight;
+//     factor = Number(math.format(factor, {precision: 14}));
+//     return factor;
+//   };
 
-  WaterSample.prototype.calcFactor = function(sample, model) {
-    var factor = 0;
-    factor += sample.chloroform * model.chloroform_weight;
-    factor += sample.bromoform * model.bromoform_weight;
-    factor += sample.bromodichloromethane * model.bromodichloromethane_weight;
-    factor += sample.dibromichloromethane * model.dibromichloromethane_weight;
-    factor = Number(math.format(factor, {precision: 14}));
-    return factor;
-  };
+// }());
 
-}());
--- client/controllers/QueryController.js
+++ client/controllers/QueryController.js
@@ -1,6 +1,7 @@
 angular.module('water.querycontroller', [])
 
 .controller('QueryController', ['$scope', '$http', function($scope, $http) {
+  $scope.data = {};
   $scope.getSample = function(id, callback) {
     console.log($scope.sample_id);
     $http({
@@ -9,13 +10,15 @@ angular.module('water.querycontroller', [])
       params: { 'id' : $scope.sample_id }
     })
     .success(function(data){
+      $scope.data = data;
       console.log(data);
       $scope.displaySample(data);
     });
   };
 
-  $scope.displaySample = function(data) {
+  $scope.displaySample = function(data, flag) {
     var $results = [];
+    console.log(data);
     if (data) {
       _.each(data, function(value, key) {
 
@@ -26,9 +29,58 @@ angular.module('water.querycontroller', [])
       $whole = $('<p></p>').text("id: " + null);
       $results.push($whole);
     }
+    if (!flag) {
+      $factorbtn = $('<form class="factorsubmit"> \
+          <div id="factorbutton"><input id="factorButton" \
+          type="submit" value="Get Factor"></div></form>');
+      $results.push($factorbtn);
+    }
     console.log($results);
     $('#holder').attr('id','sample');
     $('#sample').find('ul').html($results);
+    $(".factorsubmit").on('submit',function(event) {
+      event.preventDefault();
+      $scope.displaySample($scope.to_hash($scope.data, true), true);
+    });
+  };
 
+  $scope.to_hash = function(data, flag) {
+    var obj;
+
+    $scope.toHashAll().then(function(factors) {
+      console.log(factors);
+      var obj = _.mapObject(data, _.identity);
+
+      _.each(factors, function(scheme) {
+        var factorkey = 'factor_' + scheme.id;
+        obj[factorkey] = $scope.calcFactor(data, scheme);
+      });
+
+      console.log(obj);
+
+    });
+
+    return obj;
+
+  };
+
+  $scope.toHashAll = function(callback) {
+    return $http({
+      url: '/hash',
+      method: 'GET'
+    })
+    .then(function(results){
+        return results.data;
+    });
+  };
+
+  $scope.calcFactor = function(sample, model) {
+    var factor = 0;
+    factor += sample.chloroform * model.chloroform_weight;
+    factor += sample.bromoform * model.bromoform_weight;
+    factor += sample.bromodichloromethane * model.bromodichloromethane_weight;
+    factor += sample.dibromichloromethane * model.dibromichloromethane_weight;
+    factor = Number(math.format(factor, {precision: 14}));
+    return factor;
   };
 }]);
